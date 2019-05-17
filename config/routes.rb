5ce0b879ed7b224fcce35c65c8075982bcd39bdd
Alexander Miesen <alexmiesen@hotmail.com>
2019-05-17 12:31:32 +0200

    created a root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/destroy'
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/delete'
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/destroy'
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/delete'
+
+  root to: 'cocktails#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :new, :create, :show] do
     resources :doses, only: [:new, :create]
